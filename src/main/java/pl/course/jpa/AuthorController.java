dfb4a9a614a142be759f730d72b6ae9056ce9121
Bogusław <boguslawidzik@gmail.com>
2026-08-12 17:54:52 +0200

    boguslaw innitial commit

--- src/main/java/pl/course/jpa/AuthorController.java
+++ src/main/java/pl/course/jpa/AuthorController.java
@@ -1,10 +1,14 @@
 package pl.course.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/authors")
@@ -17,7 +21,7 @@ class AuthorController {
     }
 
     @GetMapping
-    List<AuthorDto> authorsWithNPlusOne() {
-        return authorService.findAuthorsWithNPlusOne();
+    Page<AuthorDto> authorsWithNPlusOne(@PageableDefault(size=10) Pageable pageable) {
+        return authorService.findAuthorsWithNPlusOne(pageable);
     }
 }
--- src/main/java/pl/course/jpa/AuthorService.java
+++ src/main/java/pl/course/jpa/AuthorService.java
@@ -1,5 +1,7 @@
 package pl.course.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,17 +18,22 @@ class AuthorService {
     }
 
     @Transactional(readOnly = true)
-    List<AuthorDto> findAuthorsWithNPlusOne() {
-        return authorRepository.findAll().stream()
-                .map(this::toDto)
-                .toList();
+    Page<AuthorDto> findAuthorsWithNPlusOne(Pageable pageable) {
+//        return authorRepository.findAll(pageable)
+//                .map(this::toDto);
+        Page<Author> aPages= authorRepository.findAuthorsWithBooks(pageable);
+        return aPages.map(author -> new AuthorDto(
+                author.getId(),
+                author.getName(),
+                author.getBooks().stream().map(Book::getTitle).toList()
+        ));
     }
 
-    private AuthorDto toDto(Author author) {
-        List<String> bookTitles = author.getBooks().stream()
-                .map(Book::getTitle)
-                .sorted(Comparator.naturalOrder())
-                .toList();
-        return new AuthorDto(author.getId(), author.getName(), bookTitles);
-    }
+//    private AuthorDto toDto(Author author) {
+//        List<String> bookTitles = author.getBooks().stream()
+//                .map(Book::getTitle)
+//                .sorted(Comparator.naturalOrder())
+//                .toList();
+//        return new AuthorDto(author.getId(), author.getName(), bookTitles);
+//    }
 }
