1b2808a3588a0df099cf68732626ae9c69606943
Bogusław <boguslawidzik@gmail.com>
2026-08-19 18:01:50 +0200

    19082026

--- src/main/java/pl/course/jpa/AuthorRepository.java
+++ src/main/java/pl/course/jpa/AuthorRepository.java
@@ -1,9 +1,11 @@
 package pl.course.jpa;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ interface AuthorRepository extends JpaRepository<Author, Long> {
     @Query("SELECT a FROM Author a LEFT JOIN FETCH a.books")
     List<Author> fetchAuthorsWithBooks();
 
-//    @Query("select a.id from Author a where a.name like :namePattern order by")
-//    Page<Author> findAll();
+    @Query("select a.id from Author a where a.name like :namePattern order by a.id")
+    Page<AuthorDto> findAll(@Param("namePattern") String namePattern, Pageable pageable);
 
 }
